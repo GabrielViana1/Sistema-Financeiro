691ce783afcff4291d7c22026052f243aa81ee09
GabrielViana1 <bielviana117@gmail.com>
2024-10-08 18:30:49 -0300

    Adicionando Contexto na infraestrutura

--- Infra/Configuracao/ContextBase.cs
+++ Infra/Configuracao/ContextBase.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,16 @@ using System.Threading.Tasks;
 
 namespace Infra.Configuracao
 {
-    internal class ContextBase
+    public class ContextBase : IdentityDbContext<ApplicationUser>
     {
+        public ContextBase(DbContextOptions options) : base( options)
+        {
+        }
+
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Despesa> Despesa { get; set; }
+        public DbSet<SistemaFinanceiro> SistemaFinanceiro { get; set; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { get; set; }
+
     }
 }
