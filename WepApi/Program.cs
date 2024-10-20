0911538a86adf2d396dbc2781f0b09a8870cc086
GabrielViana1 <bielviana117@gmail.com>
2024-10-19 21:36:24 -0300

    Conexão com DB e Singleton - Interface e Repositorio

--- WepApi/Program.cs
+++ WepApi/Program.cs
@@ -1,3 +1,8 @@
+using Domain.Interfaces.Generics;
+using Entities.Entidades;
+using Infra.Configuracao;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContextBase>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ContextBase>();
+
+//INTERFACE E REPOSITORIO
+builder.Services.AddSingleton(typeof(IGenerics<>), typeof(RepositoryGenerics<>));
 
 var app = builder.Build();
 
